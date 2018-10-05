cc8767cc5ea0a2ac924b3fa7102a50de0664d72d
Alain Natalini <the_klingon@live.it>
2018-10-05 09:15:43 +0200

    first commit

---
+++ NavigationDrawer/NavigationDrawer.Android/obj/Debug/android/src/md51475301b35807f08e87ca7dc112c3de7/SfNavigationDrawerRenderer.java
@@ -0,0 +1,54 @@
+package md51475301b35807f08e87ca7dc112c3de7;
+
+
+public class SfNavigationDrawerRenderer
+	extends md51558244f76c53b6aeda52c8a337f2c37.ViewRenderer_2
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Syncfusion.SfNavigationDrawer.XForms.Droid.SfNavigationDrawerRenderer, Syncfusion.SfNavigationDrawer.XForms.Android", SfNavigationDrawerRenderer.class, __md_methods);
+	}
+
+
+	public SfNavigationDrawerRenderer (android.content.Context p0, android.util.AttributeSet p1, int p2)
+	{
+		super (p0, p1, p2);
+		if (getClass () == SfNavigationDrawerRenderer.class)
+			mono.android.TypeManager.Activate ("Syncfusion.SfNavigationDrawer.XForms.Droid.SfNavigationDrawerRenderer, Syncfusion.SfNavigationDrawer.XForms.Android", "Android.Content.Context, Mono.Android:Android.Util.IAttributeSet, Mono.Android:System.Int32, mscorlib", this, new java.lang.Object[] { p0, p1, p2 });
+	}
+
+
+	public SfNavigationDrawerRenderer (android.content.Context p0, android.util.AttributeSet p1)
+	{
+		super (p0, p1);
+		if (getClass () == SfNavigationDrawerRenderer.class)
+			mono.android.TypeManager.Activate ("Syncfusion.SfNavigationDrawer.XForms.Droid.SfNavigationDrawerRenderer, Syncfusion.SfNavigationDrawer.XForms.Android", "Android.Content.Context, Mono.Android:Android.Util.IAttributeSet, Mono.Android", this, new java.lang.Object[] { p0, p1 });
+	}
+
+
+	public SfNavigationDrawerRenderer (android.content.Context p0)
+	{
+		super (p0);
+		if (getClass () == SfNavigationDrawerRenderer.class)
+			mono.android.TypeManager.Activate ("Syncfusion.SfNavigationDrawer.XForms.Droid.SfNavigationDrawerRenderer, Syncfusion.SfNavigationDrawer.XForms.Android", "Android.Content.Context, Mono.Android", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ NavigationDrawer/NavigationDrawer/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavigationDrawer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            hamburgerButton.Image = (FileImageSource)ImageSource.FromFile("hamburgericon.png");
+            List<string> list = new List<string>();
+            list.Add("Home");
+            list.Add("Page 1");
+            list.Add("Page 2");
+            listView.ItemsSource = list;
+        }
+        void hamburgerButton_Clicked(object sender, EventArgs e) => navigationDrawer.ToggleDrawer();
+
+        private async void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem.ToString() == "Home")
+            {
+                await Navigation.PushAsync(new MainPage());
+            }
+            else if (e.SelectedItem.ToString() == "Page 1")
+            {
+                await Navigation.PushAsync(new Page1());
+            }
+            else if (e.SelectedItem.ToString() == "Page 2")
+            {
+                await Navigation.PushAsync(new Page2());
+            }
+
+            navigationDrawer.ToggleDrawer();
+        }
+    }
+}
